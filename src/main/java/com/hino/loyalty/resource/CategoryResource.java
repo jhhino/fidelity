82b1a11b76fac279e78d5b787d1767efbd3a539f
Jose Henrique Hino <jh.hino@gmail.com>
2019-07-08 18:47:24 -0300

    Included DTO usage with less category info to REST

--- src/main/java/com/hino/loyalty/resource/CategoryResource.java
+++ src/main/java/com/hino/loyalty/resource/CategoryResource.java
@@ -2,6 +2,7 @@ package com.hino.loyalty.resource;
 
 import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.hino.loyalty.domain.Category;
+import com.hino.loyalty.dto.CategoryDTO;
 import com.hino.loyalty.service.CategoryService;
 
 @RestController
@@ -43,16 +45,18 @@ public class CategoryResource {
 		return ResponseEntity.noContent().build();
 	}
 	
-	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<Category>> findAll() {
-		List<Category> categoryList = categoryService.findAll();
-		
-		return ResponseEntity.ok().body(categoryList);
-	}
-	
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		categoryService.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoryDTO>> findAll() {
+		List<Category> categoryList = categoryService.findAll();
+		List<CategoryDTO> dtoList = categoryList.stream().map(obj -> new CategoryDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(dtoList);
+	}
+	
+	
 }
